ade9d9c76a90948de1bc8b7f3bdd6efa3b730340
LosBopfos <christian.lutz.home@gmx.de>
2019-03-15 20:44:59 +0100

    Added URL-Input and Bootstrap

--- monitor_app.py
+++ monitor_app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 from monitor import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return monitor_website("http://www.inspiredprogrammer.com/")
+@app.route("/", methods=('GET', 'POST'))
+def index():
+    if request.method == 'POST':
+        if request.form['url'] != '':
+            url = request.form['url']
+            return render_template('index.html', a=monitor_website('https://' + url))
+            #return monitor_website("http://www.inspiredprogrammer.com/")
+        else:
+            return render_template('index.html', a=monitor_website('https://www.google.de'))
+    else:
+        url = 'google.de'
+        return render_template('index.html', a=monitor_website('https://' + url))
