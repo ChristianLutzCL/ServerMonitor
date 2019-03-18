0bf1c8c73360ac2c707374caa54e60f856dd76a8
LosBopfos <christian.lutz.home@gmx.de>
2019-03-18 23:20:35 +0100

    Change and delete values

--- monitor_app.py
+++ monitor_app.py
@@ -9,9 +9,9 @@ def index():
     if request.method == 'POST':
         if request.form['url'] != '':
             url = request.form['url']
-            response = monitor_website('https://' + url)
+            response = monitor_website(url)
             return render_template('index.html', title="Home", response=response)
         else:
             return render_template('index.html', title="Home", response='ERROR')
     else:
-        return render_template('index.html', title="Home", response=response)
+        return render_template('index.html', title="Home")
