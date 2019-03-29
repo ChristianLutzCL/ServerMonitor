9c3d76ce582c5fb649480a8a7a8b4290864e6f02
LosBopfos <christian.lutz.home@gmx.de>
2019-03-29 23:29:04 +0100

    Update __init__.py
    Add database functionality

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '7d2e1a0f1b84b251f51ce34003689d8c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+from monitor import views
 
-import monitor.views
