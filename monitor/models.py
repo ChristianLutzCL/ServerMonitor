42f6680a7a354367f328d9822d15351bec31000f
LosBopfos <christian.lutz.home@gmx.de>
2019-03-29 23:29:55 +0100

    Create models.py

---
+++ monitor/models.py
@@ -0,0 +1,11 @@
+from monitor import db
+
+#TODO: Create Models for recent checked websites view
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
