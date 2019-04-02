d88508589aee75f1fe808a22221b0ae0c80ba406
LosBopfos <christian.lutz.home@gmx.de>
2019-04-02 23:13:14 +0200

    Update models.py
    Changed CheckedWebsite() return values and added updateDatabase function

--- monitor/models.py
+++ monitor/models.py
@@ -1,14 +1,20 @@
 from monitor import db
+from datetime import datetime
 
 
 class CheckedWebsite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #check_date = db.Column(db.DateTime)
+    check_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     website_url = db.Column(db.String(80), unique=False, nullable=True)
     response_code = db.Column(db.String(10), unique=False, nullable=True)
     response_message = db.Column(db.String(10), unique=False, nullable=True)
-    #isOnline = db.Column(db.Boolean)
 
     def __repr__(self):
-        return self.website_url
+        return '{} {} {} {}'.format(self.check_date, self.website_url, self.response_code, self.response_message)
+
+
+
+def updateDatabase(response):
+    db.session.add(response)
+    db.session.commit()
 
