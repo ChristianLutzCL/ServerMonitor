40f687f638310c6babec4b51399d996fd5822282
ChristianLutzCL <christian.lutz.privat@gmail.com>
2019-04-08 22:10:08 +0200

    Change response method

--- monitor/views.py
+++ monitor/views.py
@@ -1,25 +1,28 @@
 from flask import render_template, request
 from monitor import app
-from monitor.monitoring import monitor_website
+from monitor.monitoring import monitor_website, get_server_ip
 from monitor.models import CheckedWebsite, updateDatabase
 from sqlalchemy import desc
 
 
-
-@app.route("/", methods=('GET', 'POST'))
+@app.route("/", methods=('GET', 'POST')) #TODO: Rework whole response method
 def index():
-    if request.method == 'POST': #TODO: Rework whole response function
+    database_query = CheckedWebsite.query.order_by(desc(CheckedWebsite.check_date)).limit(10).all()
+    if request.method == 'POST':
         if request.form['url'] != '':
             url = request.form['url']
             response = monitor_website(url)
-            t = CheckedWebsite(website_url=str(response[0]), response_code=str(response[1]), response_message=str(response[2]))
-            updateDatabase(t)
-            o = CheckedWebsite.query.order_by(desc(CheckedWebsite.check_date)).limit(10).all()
-            return render_template('index.html', title="Home", response=response, test=o)
+            t = get_server_ip('inspiredprogrammer.com')
+            print(t)
+            database_return = CheckedWebsite(website_url=str(response[0]), response_code=str(response[1]), response_message=str(response[2]))
+            updateDatabase(database_return)
+            database_query = CheckedWebsite.query.order_by(desc(CheckedWebsite.check_date)).limit(10).all()
+
+            return render_template('index.html', title="Home", response=response, database_query=database_query)
         else:
-            return render_template('index.html', title="Home", response=["ERROR", "ERROR", "ERROR"])
+            return render_template('index.html', title="Home", response=["ERROR", "ERROR", "ERROR"], database_query=database_query)
     else:
-        return render_template('index.html', title="Home", response=monitor_website("https://www.google.de"))
+        return render_template('index.html', title="Home", response=monitor_website("https://www.google.de"), database_query=database_query)
 
 
 @app.route("/info", methods=('GET', 'POST'))
