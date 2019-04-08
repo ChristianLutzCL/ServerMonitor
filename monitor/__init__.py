06b3439df9b1cfba2e61822f75fe06a5bf8d1981
ChristianLutzCL <christian.lutz.privat@gmail.com>
2019-04-08 22:07:54 +0200

    Add envirionment variable

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,8 +1,9 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '7d2e1a0f1b84b251f51ce34003689d8c'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_SERVERMONITOR')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
