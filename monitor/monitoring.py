0c99ec102ff5a80b38af1d86edd2afb33c2053c5
ChristianLutzCL <christian.lutz.privat@gmail.com>
2019-04-08 22:08:32 +0200

    Add socket and get_ip function

--- monitor/monitoring.py
+++ monitor/monitoring.py
@@ -1,14 +1,15 @@
 import requests
+import socket
 
 
 def monitor_website(url):
     r = requests.get(url)
-    
+
     if r.status_code == 200:
         print(r.status_code)
         return r.url, r.status_code, r.reason
     elif r.status_code != 200:
-        print("RESPONSE FAILED")
+        print(r.status_code)
         return r.url, r.status_code, r.reason
 
 
@@ -18,3 +19,7 @@ def clear_url():
 
 def tracert(): #TODO
     pass
+
+
+def get_server_ip(url):
+    return socket.gethostbyname(url)
