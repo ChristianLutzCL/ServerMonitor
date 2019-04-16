bbba2aac69ebec2ee5cd74d3048eec686f8fdbf9
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-04-16 13:15:42 +0200

    Change ping and check_valid_url function

--- monitor/monitoring.py
+++ monitor/monitoring.py
@@ -2,10 +2,10 @@ import requests, re, socket
 
 def ping(url, prefix="https://"): #TODO: Rework ping function
 
-    if not check_valid_url(url):
-        return prefix + url
-
-    return url
+    if check_valid_url(url) != 'ERROR':
+        return url
+    else:
+        return 'https://www.inspiredprogrammer.com'
 
 
 def monitor_website(url):
@@ -13,20 +13,30 @@ def monitor_website(url):
 
     if r.status_code == 200:
         print(r.status_code)
-        return r.url, r.status_code, r.reason
+        return r.url, r.status_code, r.reason, False
     elif r.status_code != 200:
         print(r.status_code)
-        return r.url, r.status_code, r.reason
+        return r.url, r.status_code, r.reason, True
 
 
 def check_valid_url(url):
-    regex = r"((http:\/\/)|(https:\/\/)){0,1}([a-zA-Z0-9-]+\.)+([a-zA-Z])+"
+    #regex = r"((http:\/\/)|(https:\/\/)){0,1}([a-zA-Z0-9-]+\.)+([a-zA-Z])+"
+    regex = re.compile(
+    r'^(?:http|ftp)s?://' # http:// or https://
+    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
+    r'localhost|' # localhost...
+    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ...or ipv4
+    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
+    r'(?::\d+)?' # optional port
+    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
     pattern = re.compile(regex)
 
     if not pattern.match(url):
         print("Regex check failed!")
-
-    return pattern.match(url)
+        return 'ERROR'
+    else:
+        print("Regex check OK!")
+        return url
     
 
 def tracert(): #TODO tracert
