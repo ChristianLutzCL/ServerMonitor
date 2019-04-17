2ab524d09c99c29c956eaeaac7b84ec154203b32
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-04-17 22:38:45 +0200

    Change regex pattern, ping return and get_server_ip function

--- monitor/monitoring.py
+++ monitor/monitoring.py
@@ -3,7 +3,7 @@ import requests, re, socket
 def ping(url, prefix="https://"): #TODO: Rework ping function
 
     if check_valid_url(url) != 'ERROR':
-        return url
+        return prefix + check_valid_url(url)
     else:
         return 'https://www.inspiredprogrammer.com'
 
@@ -13,7 +13,7 @@ def monitor_website(url):
 
     if r.status_code == 200:
         print(r.status_code)
-        # return r.url, r.status_code, r.reason, isdown 
+        # return r.url, r.status_code, r.reason, isdown
         return r.url, r.status_code, r.reason, False 
     elif r.status_code != 200:
         print(r.status_code)
@@ -21,23 +21,18 @@ def monitor_website(url):
 
 
 def check_valid_url(url):
-    #regex = r"((http:\/\/)|(https:\/\/)){0,1}([a-zA-Z0-9-]+\.)+([a-zA-Z])+"
-    regex = re.compile(
-    r'^(?:http|ftp)s?://' # http:// or https://
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' # domain...
-    r'localhost|' # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|' # ...or ipv4
-    r'\[?[A-F0-9]*:[A-F0-9:]+\]?)' # ...or ipv6
-    r'(?::\d+)?' # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    regex = r"([a-zA-Z0-9-]+\.)+([a-zA-Z])+"
     pattern = re.compile(regex)
 
-    if not pattern.match(url):
+    spattern = pattern.search(url)
+    spgroup = spattern.group(0)
+
+    if not spgroup:
         print("Regex check failed!")
         return 'ERROR'
     else:
         print("Regex check OK!")
-        return url
+        return spgroup
     
 
 def tracert(): #TODO tracert
@@ -45,4 +40,6 @@ def tracert(): #TODO tracert
 
 
 def get_server_ip(url):
-    return socket.gethostbyname(url)
+    host = check_valid_url(url)
+
+    return socket.gethostbyname(host)
