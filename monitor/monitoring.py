abc9475d64a990a286744103c25264b078156690
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-04-19 12:56:56 +0200

    Update and add some functions

--- monitor/monitoring.py
+++ monitor/monitoring.py
@@ -1,6 +1,7 @@
-import requests, re, socket
+from monitor import app
+import requests, re, socket, json
 
-def ping(url, prefix="https://"): #TODO: Rework ping function
+def ping(url, prefix="https://"):
 
     if check_valid_url(url) != 'ERROR':
         return prefix + check_valid_url(url)
@@ -9,15 +10,17 @@ def ping(url, prefix="https://"): #TODO: Rework ping function
 
 
 def monitor_website(url):
-    r = requests.get(url)
-
-    if r.status_code == 200:
-        print(r.status_code)
-        # return r.url, r.status_code, r.reason, isdown
-        return r.url, r.status_code, r.reason, False 
-    elif r.status_code != 200:
-        print(r.status_code)
-        return r.url, r.status_code, r.reason, True 
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            print(r.status_code)
+            # return r.url, r.status_code, r.reason, isdown
+            return r.url, r.status_code, r.reason, False 
+        elif r.status_code != 200:
+            print(r.status_code)
+            return r.url, r.status_code, r.reason, True 
+    except:
+        return url, 'none', 'none', True
 
 
 def check_valid_url(url):
@@ -33,13 +36,27 @@ def check_valid_url(url):
     else:
         print("Regex check OK!")
         return spgroup
-    
 
-def tracert(): #TODO tracert
-    pass
+
+def check_latency(url):
+    try:
+        return requests.get(url).elapsed.total_seconds()
+    except:
+        return 'Timeout error'
 
 
 def get_server_ip(url):
     host = check_valid_url(url)
-
     return socket.gethostbyname(host)
+
+
+def get_server_location(ip):
+        IPSTACK_KEY = app.config['IPSTACK_API_KEY']
+        geo_ip = requests.get('http://api.ipstack.com/' + ip + "?access_key=" + IPSTACK_KEY)
+        resp = json.loads(geo_ip.text)
+
+        return resp['region_name']
+
+
+def tracert(): #TODO tracert
+    pass
