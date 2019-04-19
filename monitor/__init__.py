70779e325877d45259d5bd400222a3ac6f898ad2
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-04-19 12:56:25 +0200

    Update __init__.py

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,10 +1,17 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('FLASK_SERVERMONITOR')
+
+#Flask and DB Keys
+app.config['SECRET_KEY'] = environ.get('FLASK_SERVERMONITOR')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+#API Keys
+app.config['IPSTACK_API_KEY'] = environ.get('IPSTACK_KEY')
+
+app.config.from_envvar('APP_SETTINGS')
 db = SQLAlchemy(app)
 
 from monitor import views
