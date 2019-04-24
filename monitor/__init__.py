6b1d73cf5c0ee65a61b315d8745169b48e24eedb
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-04-24 22:10:34 +0200

    Deleted 'APP_SETTINGS'

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 #API Keys
 app.config['IPSTACK_API_KEY'] = environ.get('IPSTACK_KEY')
 
-app.config.from_envvar('APP_SETTINGS')
+#app.config.from_envvar('APP_SETTINGS')
 db = SQLAlchemy(app)
 
 from monitor import views
