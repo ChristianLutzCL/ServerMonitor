5bc58226a64c74d5f959a8bc8a80c7676aa26b8c
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-04-25 19:38:27 +0200

    Reworked views

--- monitor/views.py
+++ monitor/views.py
@@ -5,22 +5,20 @@ from monitor.models import CheckedWebsite, updateDatabase
 from sqlalchemy import desc
 
 
-@app.route("/", methods=('GET', 'POST'))
+@app.route("/", methods=['GET', 'POST'])
 def index():
     database_query = CheckedWebsite.query.order_by(desc(CheckedWebsite.check_date)).limit(10).all()
 
     if request.method == 'POST' and request.form['url'] != '':
         url = request.form['url']
-        response = monitor_website(ping(url))
-        server_ip = get_server_ip(url)
-        server_latency = check_latency(url)
-        server_loc = get_server_location(server_ip)
-        database_return = CheckedWebsite(website_url=str(response[0]), response_code=str(response[1]), response_message=str(response[2]), isdown=response[3])
+        #response = monitor_website(ping(url))# respone = [0] r.url | [1] r.status_code | [2] r.reason | [3] server_ip | [4] latency | [5] server_location | [6] isdown
+        url, status_code, status_reason, server_ip, server_latency, server_location, isdown = monitor_website(ping(url))
+        database_return = CheckedWebsite(website_url=str(url), response_code=str(status_code), response_message=str(status_reason), isdown=isdown)
         updateDatabase(database_return)
         database_query_update = CheckedWebsite.query.order_by(desc(CheckedWebsite.check_date)).limit(10).all()
-        return render_template('index.html', title="Is the website down? | ServerMonitor", response=response, ip=server_ip, lat=server_latency, loc=server_loc, database_query=database_query_update)
+        return render_template('index.html', title="Is the website down? | ServerMonitor", url=url, status_code=status_code, status_reason=status_reason, ip=server_ip, lat=server_latency, loc=server_location, isdown=isdown, database_query=database_query_update)
     else:
-        return render_template('index.html', title="Is the website down? | ServerMonitor", response=monitor_website("https://www.google.com"), lat='-', loc='-', ip='-', database_query=database_query)
+        return render_template('index.html', title="Is the website down? | ServerMonitor", url='url', status_code='status_code', status_reason='status_reason', lat='-', loc='-', ip='-', isdown='isdown', database_query=database_query)
 
 
 @app.route("/info", methods=('GET', 'POST'))
