8ef4cfc1531b3f4b7322414a0001a606128d23cf
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-05-05 00:57:21 +0200

    Added modules for authentification

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from os import environ
 
 app = Flask(__name__)
@@ -13,6 +15,10 @@ app.config['IPSTACK_API_KEY'] = environ.get('IPSTACK_KEY')
 
 #app.config.from_envvar('APP_SETTINGS')
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from monitor import views, forms
 
