a50106b32a3cbbc6b400a8e4c2e10beba44e1448
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-05-05 00:58:40 +0200

    Added User

--- monitor/models.py
+++ monitor/models.py
@@ -1,5 +1,10 @@
-from monitor import db
+from monitor import db, login_manager
+from flask_login import UserMixin
 from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class CheckedWebsite(db.Model):
@@ -12,6 +17,18 @@ class CheckedWebsite(db.Model):
 
     def __repr__(self):
         return '{} {} {} {} {}'.format(self.check_date, self.website_url, self.response_code, self.response_message, self.isdown)
+
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', {self.email}', {self.image_file}')"
 
 
 def updateDatabase(response):
