9b46c2c906b7b94385a19f506a8a2b1d02fa90b4
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-05-05 00:58:20 +0200

    Update Forms

--- monitor/forms.py
+++ monitor/forms.py
@@ -1,18 +1,29 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from monitor.models import User
 
 
 class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    password_confirm = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password')])
+    password_confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        mail = User.query.filter_by(email=email.data).first()
+        if mail:
+            raise ValidationError('That email is already taken. Please choose a different one.')
 
 
 class SignInForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField("'Remember Me")
+    remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
