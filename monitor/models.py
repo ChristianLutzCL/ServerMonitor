6a7f52561d28a00f9b4e68d62485a118d82d8175
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-05-10 22:10:22 +0200

    Add reset_token function

--- monitor/models.py
+++ monitor/models.py
@@ -1,4 +1,5 @@
-from monitor import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from monitor import db, login_manager, app
 from flask_login import UserMixin
 from datetime import datetime
 
@@ -26,6 +27,19 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(20), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', {self.email}', {self.image_file}')"
