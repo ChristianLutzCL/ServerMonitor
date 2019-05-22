d9642072ff3382ace850ac752d03e76871eee679
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-05-22 22:58:09 +0200

    Fix login_view bug

--- monitor/__init__.py
+++ monitor/__init__.py
@@ -9,7 +9,7 @@ from monitor.config import Config
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 mail = Mail()
