acbe410f5d3298f2fd091ec1b897a5b41accdb52
ChristianLutzCL <chris@inspiredprogrammer.com>
2019-05-24 19:06:54 +0200

    Update for pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,14 @@
 import pytest
-from monitor.monitoring import func
+from monitor.monitoring import func, monitor_website, check_latency
+
+
 def test_answer():
     assert func(5) == 5
+
+def test_check_latency():
+    url = 'https://monitor.inspiredprogrammer.com'
+    assert check_latency(url) != 'Timeout error'
+
+def test_monitor_website():
+    url = "https://monitor.inspiredprogrammer.com"
+    assert monitor_website(url) == None
